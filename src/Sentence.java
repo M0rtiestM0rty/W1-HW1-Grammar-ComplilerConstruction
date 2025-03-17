f8d75f941b2724d3de638a15639dac167d22daa0
MortiestMorty <d.arroyo14@student.keiseruniversity.edu>
2025-03-16 23:23:47 -0400

    OG

--- src/Sentence.java
+++ src/Sentence.java
@@ -1,4 +1,164 @@
-package PACKAGE_NAME;
+/*1.	Create a class named Sentence.
+In the Sentence class, declare the following seven
+private attributes of type String:
+    subjectMarker
+ 	adjective
+ 	subject
+ 	adverb
+ 	verb
+ 	directObjectMarker
+ 	directObject
+2.	Also declare a private array named sentenceTable
+containing seven String elements with the indexes of
+the array having the following designations:
+ 	0 – subject marker
+ 	1 – adjective
+ 	2 – subject
+ 	3 – adverb
+ 	4 – verb
+ 	5 – direct object marker
+ 	6 – direct object
+3.	Create a constructor for the Sentence class.
+ 	The constructor takes one parameter, a character string.
+ 	It calls the parse() method and passes the character string
+ 	to it.
+4.	In the Sentence class, create the public methods described
+    below.he parse() method takes a character string as a parameter and
+    returns void.  It parses the words in the character string into the
+    respective seven private attributes of the Sentence class.  It
+    assumes that the contents of the string parameter are correctly
+    formatted.No standard Java API methods are used to parse the string.
+    Instead, just basic Java coding for reading a character and
+    building a character string is used to perform the parsing
+    algorithm.The toString() method takes no parameters.  It uses
+    the contents of the seven Sentence attributes to create a
+    string with a single space between each attribute.  The order
+    of the attributes is subject marker, adjective, subject, adverb,
+    verb, direct object marker, and direct object.  It then returns
+    the string.The shuffle() method takes no parameters and returns
+    void.  It first loads the contents of the seven Sentence
+    attributes into the sentenceTable array, and does so in their
+    original order. It then uses a random number generator to shuffle
+    the contents of the elements in the sentenceTable.The
+    getSentenceTableContents() method takes no parameters.
+    It uses the contents of the sentenceTable to create a string
+    with a single space between each element.  In the string, the
+    elements should be placed in order from 0 to 6.  It then returns
+    the string.
 
-public class Sentence {
-}
+*/
+import java.util.Random;
+public class Sentence
+{
+    private String subjectMarker;
+    private String adjective;
+    private String subject;
+    private String adverb;
+    private String verb;
+    private String directObjectMarker;
+    private String directObject;
+    // attributes
+    private String [] sentenceTable;
+    // array
+
+    public Sentence(String input)
+    {
+        sentenceTable = new String[7];
+        parse(input);
+    }// end Constructor
+
+    private void parse (String input)
+    {
+        int index = 0, wordIndex = 0;
+        char [] chars = input.toCharArray();
+        StringBuilder word = new StringBuilder();
+
+        while (index < chars.length && wordIndex < 7)
+        {
+            char c = chars[index];
+
+            if (c == ' ')
+            {
+                if (word.length() > 0)
+                {
+                    assignWord (word.toString(), wordIndex);
+                    wordIndex++;
+                    word.setLength(0);
+                    // space for nest word
+                }// end nested if loop
+            }// end outer if loop
+            else
+            {
+                word.append(c);
+            }// end
+
+            index++;
+        }// end while loop
+        if (word.length() > 0 && wordIndex < 7)
+        {
+            assignWord(word.toString(), wordIndex);
+        }// end if
+    }// end private parse method
+
+    private void assignWord (String word, int index)
+    {
+        switch (index)
+        {
+            case 0:
+                subjectMarker = word;
+                break;
+            case 1:
+                adjective = word;
+                break;
+            case 2:
+                subject = word;
+                break;
+            case 3:
+                adverb = word;
+                break;
+            case 4:
+                verb = word;
+                break;
+            case 5:
+                directObjectMarker = word;
+                break;
+            case 6:
+                directObject = word;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid index" + index);
+        }// end switch
+    }// end assignWord Method
+
+    //sentence back to string
+    public String toString()
+    {
+        return String.join(" ", subjectMarker,
+                adjective,subject,adverb,verb, directObjectMarker,
+                directObject);
+    }// end toString method
+
+    public void shuffle()
+    {
+        sentenceTable = new String[] {subjectMarker, adjective,
+                subject, adverb, verb, directObjectMarker,};
+
+        Random rand = new Random();
+        for(int a = sentenceTable.length - 1; a >= 0; a--)
+        {
+            int b = rand.nextInt(a + 1);
+            String temp = sentenceTable[b];
+            sentenceTable[a] = sentenceTable[b];
+            sentenceTable[b] = temp;
+        }// end for loop
+    }// end shuffle method
+
+    public static void main (String[] args)
+    {
+        Sentence sentence = new Sentence("The quick brown fox jumps over the lazy dog");
+
+        System.out.println("Original Sentence" + sentence);
+        sentence.shuffle();
+        System.out.println("Shuffled Sentence" + sentence);
+    }// end main
+}// end Class
--- src/SentenceDriver.java
+++ src/SentenceDriver.java
@@ -1,4 +1,110 @@
-package PACKAGE_NAME;
+/*Create a class named SentenceDriver that demonstrates
+the use of the Sentence class by presenting a menu that
+allows a user to do the following options and implements
+each of these features.
+ 	(1) Enter a sentence
+ 	(2) Shuffle the sentence
+ 	(3) Display the original sentence
+ 	(4) Display the sentence table contents
+ 	(5) Exit the program
+ 	Enter a menu option (0 for menu):
 
-public class SentenceDriver {
-}
+An example of a valid input sentence would be
+“The large bear slowly chases a fox”.  Note that the driver
+program shall only instantiate a Sentence object when a user
+chooses the ‘1’ option to enter a sentence.  The ‘2’, ‘3’, and ‘4’
+options shall display an error message if no sentence has been
+entered yet.
+*/
+import java.util.Scanner;
+public class SentenceDriver
+{
+    public static void main (String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        Sentence sentence = null;
+        int choice;
+
+        displayMenu();
+
+        while (true) {
+            System.out.print("Enter Menu Option: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid Menu Option");
+                scanner.nextLine();
+            }// end while scanner
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    displayMenu();
+                    break;
+                case 1:
+                    System.out.print("Enter Sentence(input requires 7 words): ");
+                    String input = scanner.nextLine();
+                    String[] words = input.split(" ");
+
+                    if (words.length == 7)
+                    {
+                        sentence = new Sentence(input);
+                        System.out.println("Sentence Stored: ");
+                    }// end if
+                    else
+                    {
+                        System.out.println("Invalid Sentence");
+                    }// end else
+
+                    break;
+                case 2:
+                    if ( sentence == null) {
+                        System.out.println("No input entered: ");
+                    }// end if
+                    else {
+                        sentence.shuffle();
+                        System.out.println("Sentence Shuffled: ");
+                    }
+                    break;
+                case 3:
+                    if ( sentence == null) {
+                        System.out.println("No input entered: ");
+                    }// end if
+                    else
+                    {
+                        System.out.println("Original Sentence: " + sentence);
+                    }// end else
+                    break;
+                case 4:
+                    if ( sentence == null)
+                    {
+                        System.out.println("No input entered: ");
+                    }// end if
+                    else
+                    {
+                        System.out.println("Sentence Table Contents: " + sentence.getClass());
+                    }// end else
+                    break;
+                case 5:
+                    System.out.println("Exiting Program");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid Menu Option: enter a number between 0 and 5 ");
+            }// end switch
+        }// end while
+    }// end main
+
+    private static void displayMenu()
+    {
+        System.out.println("Menu");
+        System.out.println("1 - Enter a Sentence");
+        System.out.println("2 - Shuffle");
+        System.out.println("3 - Display Original Sentence");
+        System.out.println("4 - Display Sentence Table Contents");
+        System.out.println("5 - Exit");
+        System.out.print("0 - Display Menu Options again ");
+    }// end display menu
+}// end class
